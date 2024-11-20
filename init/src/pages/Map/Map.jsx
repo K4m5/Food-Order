e2fa0e0c58fcf5c4c5b2e7d52b3c2b63d260ed19
K4m5 <doquan15012002@gmail.com>
2024-11-20 23:11:55 +0700

    update

--- init/src/pages/Map/Map.jsx
+++ init/src/pages/Map/Map.jsx
@@ -47,6 +47,12 @@ const Map = () => {
     iconAnchor: [16, 32], // point of the icon which will correspond to marker's location
     popupAnchor: [0, -32], // point from which the popup should open relative to the iconAnchor
   });
+  const homeIcon = L.icon({
+    iconUrl: "/home.png",
+    iconSize: [32, 32], // size of the icon
+    iconAnchor: [16, 32], // point of the icon which will correspond to marker's location
+    popupAnchor: [0, -32], // point from which the popup should open relative to the iconAnchor
+  });
   const center = [20.98177624011233, 105.79562539037852];
   const handleMarkerClick = (id) => {
     setSelectedOrder(true);
@@ -129,7 +135,7 @@ const Map = () => {
               </React.Fragment>
             ))}
             <Marker
-              icon={customIcon} // Use custom icon
+              icon={homeIcon} // Use custom icon
               position={[center[0], center[1]]}
             >
               <Popup>
@@ -148,80 +154,76 @@ const Map = () => {
                 Chi tiết đơn hàng
               </div>
               <div className="order-body">
-                {order?.detailOrders?.map((o, index) => (
-                  <div key={index} className="pb-3">
-                    <section className="bg-white osahan-main-body rounded shadow-sm overflow-hidden">
-                      <div className="osahan-status">
-                        <div className="p-3 status-order bg-white border-bottom d-flex align-items-center">
-                          <p className="m-0">
-                            <i className="feather-calendar text-primary"></i>{" "}
-                            {new Date(
-                              order?.order?.updated_at
-                            ).toLocaleString()}
-                          </p>
-                        </div>
+                <div className="pb-3">
+                  <section className="bg-white osahan-main-body rounded shadow-sm overflow-hidden">
+                    <div className="osahan-status">
+                      <div className="p-3 status-order bg-white border-bottom d-flex align-items-center">
+                        <p className="m-0">
+                          <i className="feather-calendar text-primary"></i>{" "}
+                          {new Date(order?.order?.updated_at).toLocaleString()}
+                        </p>
+                      </div>
 
-                        <div className="p-3 border-bottom bg-white">
-                          <h6 className="font-weight-bold">Địa chỉ</h6>
-                          <p className="m-0 small">{order.order?.address}</p>
-                        </div>
-                        <div className="p-3 border-bottom">
-                          <p className="font-weight-bold   mb-1">Đơn hàng</p>
+                      <div className="p-3 border-bottom bg-white">
+                        <h6 className="font-weight-bold">Địa chỉ</h6>
+                        <p className="m-0 small">{order.order?.address}</p>
+                      </div>
+                      <div className="p-3 border-bottom">
+                        <p className="font-weight-bold   mb-1">Đơn hàng</p>
 
-                          {order.detailOrders?.map((item, index) => (
-                            <Fragment key={index}>
-                              <img
-                                alt="#"
-                                src={item.food.image}
-                                className="img-fluid sc-osahan-logo mr-2"
-                              />{" "}
-                              <span className="text-primary font-weight-bold d-flex align-items-center justify-content-between ">
-                                <div>{item.food.name} </div>
-                                <div>{formatMoney(item.food.price)}</div>
-                              </span>
-                              <span className="text-muted small">
-                                Số lượng: {item.quantity}
-                              </span>
-                              {item.toppings.map((topping, i) => (
-                                <div
-                                  key={i}
-                                  className="gold-members d-flex align-items-center justify-content-between   py-2 border-bottom"
-                                >
-                                  <div className="media align-items-center">
-                                    <div className="mr-2">&middot;</div>
-                                    <div className="media-body">
-                                      <p className="m-0"> {topping.name} </p>
-                                    </div>
-                                  </div>
-                                  <div className="d-flex align-items-center">
-                                    <p className="text-gray mb-0 float-right ml-2 text-muted small">
-                                      {formatMoney(topping.price)}
-                                    </p>
+                        {order.detailOrders?.map((item, index) => (
+                          <Fragment key={index}>
+                            <img
+                              alt="#"
+                              src={item.food.image}
+                              className="img-fluid sc-osahan-logo mr-2"
+                            />{" "}
+                            <span className="text-primary font-weight-bold d-flex align-items-center justify-content-between ">
+                              <div>{item.food.name} </div>
+                              <div>{formatMoney(item.food.price)}</div>
+                            </span>
+                            <span className="text-muted small">
+                              Số lượng: {item.quantity}
+                            </span>
+                            {item.toppings.map((topping, i) => (
+                              <div
+                                key={i}
+                                className="gold-members d-flex align-items-center justify-content-between   py-2 border-bottom"
+                              >
+                                <div className="media align-items-center">
+                                  <div className="mr-2">&middot;</div>
+                                  <div className="media-body">
+                                    <p className="m-0"> {topping.name} </p>
                                   </div>
                                 </div>
-                              ))}
-                            </Fragment>
-                          ))}
-                        </div>
-
-                        <div className="p-3 bg-white">
-                          <div className="d-flex align-items-center mb-2">
-                            <h6 className="font-weight-bold mb-1">Tổng tiền</h6>
-                            <h6 className="font-weight-bold ml-auto mb-1">
-                              {formatMoney(order.order?.amount)}
-                            </h6>
-                          </div>
-                          <p className="m-0 small text-muted">
-                            Bạn có thể kiểm tra chi tiết đơn hàng của mình tại
-                            đây,
-                            <br />
-                            Cảm ơn bạn đã đặt hàng.
-                          </p>
-                        </div>
+                                <div className="d-flex align-items-center">
+                                  <p className="text-gray mb-0 float-right ml-2 text-muted small">
+                                    {formatMoney(topping.price)}
+                                  </p>
+                                </div>
+                              </div>
+                            ))}
+                          </Fragment>
+                        ))}
                       </div>
-                    </section>
-                  </div>
-                ))}
+
+                      <div className="p-3 bg-white">
+                        <div className="d-flex align-items-center mb-2">
+                          <h6 className="font-weight-bold mb-1">Tổng tiền</h6>
+                          <h6 className="font-weight-bold ml-auto mb-1">
+                            {formatMoney(order.order?.amount)}
+                          </h6>
+                        </div>
+                        <p className="m-0 small text-muted">
+                          Bạn có thể kiểm tra chi tiết đơn hàng của mình tại
+                          đây,
+                          <br />
+                          Cảm ơn bạn đã đặt hàng.
+                        </p>
+                      </div>
+                    </div>
+                  </section>
+                </div>
               </div>
             </div>
           )}
